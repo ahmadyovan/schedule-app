1e3904c19724363c5b0c9470a8a2e86f6eee96d1
AhmadYovanArdiansyah <ahmadyovanardiansyah@gmail.com>
2024-06-17 18:41:32 +0700

    .

--- app/dosen/page.tsx
+++ app/dosen/page.tsx
@@ -5,7 +5,9 @@ export default function DosenPage () {
         <div className="h-screen w-screen">
             <Navbar />
             <div className="h-[90%] w-full bg-green-200 flex flex-col items-center justify-center gap-32 text-4xl pb-28 font-semibold">
-
+                <div>
+                    
+                </div>
             </div>
         </div>
     )
