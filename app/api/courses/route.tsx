b2dc41664cf84c082e1acbad649e643eed8ce4e5
AhmadYovanArdiansyah <ahmadyovanardiansyah@gmail.com>
2024-06-27 13:15:42 +0700

    ..

--- app/api/courses/route.tsx
+++ app/api/courses/route.tsx
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const FLASK_SERVER_URL = 'https://yovan.pythonanywhere.com/';
+//const FLASK_SERVER_URL = 'http://127.0.0.1:5000/';
 
 export async function POST(request: Request) {
   try {
