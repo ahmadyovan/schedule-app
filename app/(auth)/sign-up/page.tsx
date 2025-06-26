098586701958cfedadb24f115509ac7d2b8290ee
ahmadyovan <ahmadyovanardiansyah@gmail.com>
2025-06-26 22:35:13 +0700

    dd

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,42 +1,43 @@
-// 'use client'
+'use client'
 
-// import { useState } from "react"
+import { useState } from "react"
 
-// const Home = () => {
+const Home = () => {
     
-//     const [file, setFile] = useState<File | null>(null);
+    const [file, setFile] = useState<File | null>(null);
 
-//   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const selected = e.target.files?.[0] || null;
-//     setFile(selected);
-//   };
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selected = e.target.files?.[0] || null;
+    setFile(selected);
+  };
 
-//   const handleUpload = async () => {
-//     if (!file) return;
+  const handleUpload = async () => {
+    if (!file) return;
+    return;
 
-//     const formData = new FormData();
-//     formData.append('file', file);
+    // const formData = new FormData();
+    // formData.append('file', file);
 
-//     const res = await fetch('/api/import-dosen', {
-//       method: 'POST',
-//       body: formData,
-//     });
+    // const res = await fetch('/api/import-dosen', {
+    //   method: 'POST',
+    //   body: formData,
+    // });
 
-//     const result = await res.json();
-//     console.log(result);
-//   };
+    // const result = await res.json();
+    // console.log(result);
+  };
 
-//   return (
-//     <div className="p-4">
-//       <input type="file" accept=".csv" onChange={handleFileChange} />
-//       <button
-//         onClick={handleUpload}
-//         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
-//       >
-//         Upload CSV
-//       </button>
-//     </div>
-//   );
-// }
+  return (
+    <div className="p-4">
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <button
+        onClick={handleUpload}
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
+      >
+        Upload CSV
+      </button>
+    </div>
+  );
+}
 
-// export default Home
+export default Home
